1bf480537fa4ee366f864a234de3878ad79f2f17
Doug Woodward <doug@fourthpin.com>
2024-06-07 21:34:17 -0700

    add data pull script

---
+++ pull_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+youtube-dl youtube-dl https://www.youtube.com/watch\?v\=N6pa2gDwRYY # south bay surf b jacobson 
+
+
+ffmpeg -i south_bay_surf_april_2018_bjacobson.mp4 south_bay_surf_frame_image%5d.png
