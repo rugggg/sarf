0857f7288268218923e7d387a027b9fa059528d0
Doug Woodward <doug@fourthpin.com>
2024-06-10 21:42:40 -0700

    add some of the tutorial boilerplate code, net runs up to loss, has shape mismatch

--- net.py
+++ net.py
@@ -1,9 +1,14 @@
+""" heavily based on: https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/JAX/tutorial9/AE_CIFAR10.html"""
 import numpy as np
 import jax
 import jax.numpy as jnp
 import optax
+import os
+from tqdm import tqdm
+from torch.utils.tensorboard.writer import SummaryWriter
 from flax import linen as nn
 from jax import random
+from flax.training import train_state, checkpoints
 # Seeding for random operations
 main_rng = random.PRNGKey(42)
 
@@ -14,6 +19,7 @@ import torch.utils.data as data
 import torchvision
 from torchvision.datasets import CIFAR10
 
+CHECKPOINT_PATH = "./checkpoint/"
 class WaveSegEncoder(nn.Module):
 
     latent_dim: int
@@ -83,6 +89,20 @@ class WaveSegSegment(nn.Module):
         self.encoder = WaveSegEncoder(latent_dim=128)
         self.decoder = WaveSegDecoder(latent_dim=128, c_out=3)
 
+class Autoencoder(nn.Module):
+    c_hid: int
+    latent_dim : int
+
+    def setup(self):
+        # Alternative to @nn.compact -> explicitly define modules
+        # Better for later when we want to access the encoder and decoder explicitly
+        self.encoder = WaveSegEncoder(latent_dim=self.latent_dim)
+        self.decoder = WaveSegDecoder(latent_dim=self.latent_dim, c_out=3)
+
+    def __call__(self, x):
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
 
 def encoder_test():
     ## Test encoder implementation
@@ -143,6 +163,147 @@ def numpy_collate(batch):
     else:
         return np.array(batch)
 
+class GenerateCallback:
+
+    def __init__(self, input_imgs, every_n_epochs=1):
+        super().__init__()
+        self.input_imgs = input_imgs  # Images to reconstruct during training
+        self.every_n_epochs = every_n_epochs  # Only save those images every N epochs (otherwise tensorboard gets quite large)
+
+    def log_generations(self, model, state, logger, epoch):
+        if epoch % self.every_n_epochs == 0:
+            reconst_imgs = model.apply({'params': state.params}, self.input_imgs)
+            reconst_imgs = jax.device_get(reconst_imgs)
+
+            # Plot and add to tensorboard
+            imgs = np.stack([self.input_imgs, reconst_imgs], axis=1).reshape(-1, *self.input_imgs.shape[1:])
+            imgs = jax_to_torch(imgs)
+            grid = torchvision.utils.make_grid(imgs, nrow=2, normalize=True, value_range=(-1,1))
+            logger.add_image("Reconstructions", grid, global_step=epoch)
+
+
+
+class TrainerModule:
+
+    def __init__(self, c_hid, latent_dim, lr=1e-3, seed=42):
+        super().__init__()
+        self.c_hid = c_hid
+        self.latent_dim = latent_dim
+        self.lr = lr
+        self.seed = seed
+        # Create empty model. Note: no parameters yet
+        self.model = Autoencoder(c_hid=self.c_hid, latent_dim=self.latent_dim)
+        # Prepare logging
+        self.exmp_imgs = next(iter(val_loader))[0][:8]
+        self.log_dir = os.path.join(CHECKPOINT_PATH, f'cifar10_{self.latent_dim}')
+        self.generate_callback = GenerateCallback(self.exmp_imgs, every_n_epochs=50)
+        self.logger = SummaryWriter(log_dir=self.log_dir)
+        # Create jitted training and eval functions
+        self.create_functions()
+        # Initialize model
+        self.init_model()
+
+    def create_functions(self):
+        # Training function
+        def train_step(state, batch):
+            loss_fn = lambda params: mse_recon_loss(self.model, params, batch)
+            loss, grads = jax.value_and_grad(loss_fn)(state.params)  # Get loss and gradients for loss
+            state = state.apply_gradients(grads=grads)  # Optimizer update step
+            return state, loss
+        self.train_step = jax.jit(train_step)
+        # Eval function
+        def eval_step(state, batch):
+            return mse_recon_loss(self.model, state.params, batch)
+        self.eval_step = jax.jit(eval_step)
+
+    def init_model(self):
+        # Initialize model
+        rng = jax.random.PRNGKey(self.seed)
+        rng, init_rng = jax.random.split(rng)
+        params = self.model.init(init_rng, self.exmp_imgs)['params']
+        # Initialize learning rate schedule and optimizer
+        lr_schedule = optax.warmup_cosine_decay_schedule(
+            init_value=0.0,
+            peak_value=1e-3,
+            warmup_steps=100,
+            decay_steps=500*len(train_loader),
+            end_value=1e-5
+        )
+        optimizer = optax.chain(
+            optax.clip(1.0),  # Clip gradients at 1
+            optax.adam(lr_schedule)
+        )
+        # Initialize training state
+        self.state = train_state.TrainState.create(apply_fn=self.model.apply, params=params, tx=optimizer)
+
+    def train_model(self, num_epochs=500):
+        # Train model for defined number of epochs
+        best_eval = 1e6
+        for epoch_idx in tqdm(range(1, num_epochs+1)):
+            self.train_epoch(epoch=epoch_idx)
+            if epoch_idx % 10 == 0:
+                eval_loss = self.eval_model(val_loader)
+                self.logger.add_scalar('val/loss', eval_loss, global_step=epoch_idx)
+                if eval_loss < best_eval:
+                    best_eval = eval_loss
+                    self.save_model(step=epoch_idx)
+                self.generate_callback.log_generations(self.model, self.state, logger=self.logger, epoch=epoch_idx)
+                self.logger.flush()
+
+    def train_epoch(self, epoch):
+        # Train model for one epoch, and log avg loss
+        losses = []
+        for batch in train_loader:
+            self.state, loss = self.train_step(self.state, batch)
+            losses.append(loss)
+        losses_np = np.stack(jax.device_get(losses))
+        avg_loss = losses_np.mean()
+        self.logger.add_scalar('train/loss', avg_loss, global_step=epoch)
+
+    def eval_model(self, data_loader):
+        # Test model on all images of a data loader and return avg loss
+        losses = []
+        batch_sizes = []
+        for batch in data_loader:
+            loss = self.eval_step(self.state, batch)
+            losses.append(loss)
+            batch_sizes.append(batch[0].shape[0])
+        losses_np = np.stack(jax.device_get(losses))
+        batch_sizes_np = np.stack(batch_sizes)
+        avg_loss = (losses_np * batch_sizes_np).sum() / batch_sizes_np.sum()
+        return avg_loss
+
+    def save_model(self, step=0):
+        # Save current model at certain training iteration
+        checkpoints.save_checkpoint(ckpt_dir=self.log_dir, target=self.state.params, prefix=f'cifar10_{self.latent_dim}_', step=step)
+
+    def load_model(self, pretrained=False):
+        # Load model. We use different checkpoint for pretrained models
+        if not pretrained:
+            params = checkpoints.restore_checkpoint(ckpt_dir=self.log_dir, target=self.state.params, prefix=f'cifar10_{self.latent_dim}_')
+        else:
+            params = checkpoints.restore_checkpoint(ckpt_dir=os.path.join(CHECKPOINT_PATH, f'cifar10_{self.latent_dim}.ckpt'), target=self.state.params)
+        self.state = train_state.TrainState.create(apply_fn=self.model.apply, params=params, tx=self.state.tx)
+
+    def checkpoint_exists(self):
+        # Check whether a pretrained model exist for this autoencoder
+        return os.path.isfile(os.path.join(CHECKPOINT_PATH, f'cifar10_{self.latent_dim}.ckpt'))
+
+def train_cifar(latent_dim, train_loader, test_loader):
+    # Create a trainer module with specified hyperparameters
+    trainer = TrainerModule(c_hid=32, latent_dim=latent_dim)
+    if not trainer.checkpoint_exists():  # Skip training if pretrained model exists
+        trainer.train_model(num_epochs=500)
+        trainer.load_model()
+    else:
+        trainer.load_model(pretrained=True)
+    test_loss = trainer.eval_model(test_loader)
+    # Bind parameters to model for easier inference
+    trainer.model_bd = trainer.model.bind({'params': trainer.state.params})
+    return trainer, test_loss
+
+
+
 if __name__ == "__main__":
 
     # Loading the training dataset. We need to split it into a training and validation part
@@ -151,7 +312,20 @@ if __name__ == "__main__":
 
 
     # data
-    train_loader = data.DataLoader(train_set, batch_size=256, shuffle=True, drop_last=True, pin_memory=True, num_workers=4, collate_fn=numpy_collate, persistent_workers=True)
+    train_dataset = CIFAR10(root='./cifar/', train=True, transform=image_to_numpy, download=True)
+    train_set, val_set = data.random_split(train_dataset, [45000, 5000], generator=torch.Generator().manual_seed(42))
 
+    # Loading the test set
+    test_set = CIFAR10(root='./cifar/', train=False, transform=image_to_numpy, download=True)
+
+    # We define a set of data loaders that we can use for various purposes later.
+    train_loader = data.DataLoader(train_set, batch_size=256, shuffle=True, drop_last=True, pin_memory=True, num_workers=4, collate_fn=numpy_collate, persistent_workers=True)
+    val_loader = data.DataLoader(val_set, batch_size=256, shuffle=False, drop_last=False, num_workers=4, collate_fn=numpy_collate)
+    test_loader = data.DataLoader(test_set, batch_size=256, shuffle=False, drop_last=False, num_workers=4, collate_fn=numpy_collate)
+    
     encoder_test()
     decoder_test()
+
+    train_cifar(128, train_loader, test_loader)
+
+
