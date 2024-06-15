4327feea345eb75f002b51170cc62bbfa59054fe
Doug Woodward <doug@fourthpin.com>
2024-06-14 22:35:42 -0700

    updated autosam and some example masks

--- autosam.py
+++ autosam.py
@@ -3,10 +3,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import gc
+import os
+import tqdm
 from transformers import pipeline
 from PIL import Image
-import requests
-
+import glob
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
@@ -19,23 +20,27 @@ def show_mask(mask, ax, random_color=False):
     del mask
     gc.collect()
 
-def show_masks_on_image(raw_image, masks):
+def show_masks_on_image(raw_image, masks, title=None, save=False):
   plt.imshow(np.array(raw_image))
   ax = plt.gca()
   ax.set_autoscale_on(False)
   for mask in masks:
       show_mask(mask, ax=ax, random_color=True)
   plt.axis("off")
+  ax.set_title(title, fontsize=16, fontweight='bold', fontfamily='monospace', color='r')
+  if save:
+      plt.savefig(im_path.replace(".png", "_masked.png").replace("data", "masks"), bbox_inches='tight')
   plt.show()
   del mask
   gc.collect()
 
 generator = pipeline("mask-generation", model="facebook/sam-vit-huge", device="cpu")
-img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg"
-raw_image = Image.open("./data/south_bay_surf_frame_image00798.png").convert("RGB")
 
-plt.imshow(raw_image)
-outputs = generator(raw_image, points_per_batch=16)
-masks = outputs["masks"]
-show_masks_on_image(raw_image, masks)
-
+imgs = glob.glob("./data/south_bay_surf_frame_image*.png")
+for i in tqdm.tqdm(range(0, len(imgs), 400)):
+    im_path = imgs[i]
+    raw_image = Image.open(im_path).convert("RGB")
+    plt.imshow(raw_image)
+    outputs = generator(raw_image, points_per_batch=32)
+    masks = outputs["masks"]
+    show_masks_on_image(raw_image, masks, title=os.path.basename(im_path), save=True)
