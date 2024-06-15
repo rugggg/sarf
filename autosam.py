c0bd3cee984c7783fdbcaebc7b45ce863edb760e
Doug Woodward <doug@fourthpin.com>
2024-06-14 22:12:15 -0700

    add auto SAM and add reqs

---
+++ autosam.py
@@ -0,0 +1,41 @@
+# https://github.com/huggingface/notebooks/blob/main/examples/automatic_mask_generation.ipynb
+
+import numpy as np
+import matplotlib.pyplot as plt
+import gc
+from transformers import pipeline
+from PIL import Image
+import requests
+
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    del mask
+    gc.collect()
+
+def show_masks_on_image(raw_image, masks):
+  plt.imshow(np.array(raw_image))
+  ax = plt.gca()
+  ax.set_autoscale_on(False)
+  for mask in masks:
+      show_mask(mask, ax=ax, random_color=True)
+  plt.axis("off")
+  plt.show()
+  del mask
+  gc.collect()
+
+generator = pipeline("mask-generation", model="facebook/sam-vit-huge", device="cpu")
+img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg"
+raw_image = Image.open("./data/south_bay_surf_frame_image00798.png").convert("RGB")
+
+plt.imshow(raw_image)
+outputs = generator(raw_image, points_per_batch=16)
+masks = outputs["masks"]
+show_masks_on_image(raw_image, masks)
+
